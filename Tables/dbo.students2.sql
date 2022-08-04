334d71a0b8b0c5ecd73e195fc4221cd1e925dbff
Julio Herrera <jcherrera@bgcapitalgroup.mx>
2022-08-04 16:31:48 -0500

    Add column Prueba2

--- Tables/dbo.students2.sql
+++ Tables/dbo.students2.sql
@@ -4,7 +4,8 @@
   [Email] [varchar](100) NULL,
   [Class] [decimal](8, 2) NULL,
   [Pesos] [decimal](8, 2) NULL,
-  [Prueba] [varbinary](50) NULL
+  [Prueba] [varbinary](50) NULL,
+  [Prueba2] [varchar](50) NULL
 )
 ON [PRIMARY]
 GO
